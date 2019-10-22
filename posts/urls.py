9f3404269feb3d257b67507a75434920ef884781
MizanUrp11 <mrn1105009@gmail.com>
2019-10-22 22:55:41 +0600

    Create post

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
-from .views import HomePageView,PostDetailView
+from .views import HomePageView,PostDetailView,BlogCreateView
 
 from django.urls import path
 
 urlpatterns = [
+    path('post/new/', BlogCreateView.as_view(),name = 'post_new'),
     path('post/<int:pk>/', PostDetailView.as_view(), name = 'post_detail'),
     path('', HomePageView.as_view(),name = 'home'),
 ]
