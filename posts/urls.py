b8009778ec17387a7130eb42eb8654618dcbb28d
MizanUrp11 <mrn1105009@gmail.com>
2019-10-22 08:49:35 +0600

    Css setup

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
-from .views import HomePageView
+from .views import HomePageView,PostDetailView
 
 from django.urls import path
 
 urlpatterns = [
+    path('post/<int:pk>/', PostDetailView.as_view(), name = 'post_detail'),
     path('', HomePageView.as_view(),name = 'home'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -9,3 +9,7 @@ class HomePageView(ListView):
     template_name = 'home.html'
     context_object_name = 'all_blog_posts'
     
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
