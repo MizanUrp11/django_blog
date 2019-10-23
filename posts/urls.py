35eb304600df175e91eace243d8c4ad6c919ea28
MizanUrp11 <mrn1105009@gmail.com>
2019-10-23 21:49:48 +0600

    Post delete

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
-from .views import HomePageView,PostDetailView,BlogCreateView, PostUpdateView
+from .views import HomePageView,PostDetailView,BlogCreateView, PostUpdateView, PostDeleteView
 
 from django.urls import path
 
 urlpatterns = [
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(),name = 'post_delete'),
     path('post/<int:pk>/edit', PostUpdateView.as_view(), name = 'post_edit'),
     path('post/new/', BlogCreateView.as_view(),name = 'post_new'),
     path('post/<int:pk>/', PostDetailView.as_view(), name = 'post_detail'),
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -24,3 +25,8 @@ class PostUpdateView(UpdateView):
     model = Post
     template_name = "post_edit.html"
     fields = ['title', 'body']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy('home')
